8ab86c0e6928bb6ff95dec23057906787908422b
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-07-26 11:15:57 -0500

    showing error messsages when fields are empty and when user clicks send button and any field is empty

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -23,6 +23,20 @@ export class TemplateComponent implements OnInit {
 
   public save( forma: NgForm ) {
 
+    // Esto es para que al dar clic al botón de Enviar y mientras algun campo no sea valido aparezcan los mensajes de error poniendo los controls de la forma como Touched ( es decir, como si hubiesen sido tocados por el usuario )
+    if( forma.invalid ) {
+
+      Object.values( forma.controls ).forEach( control =>{
+
+        //console.log( control );
+
+        control.markAllAsTouched();
+
+      } );
+
+
+    }
+
     
     console.log(' Submit disparado ');
     console.log( forma );
